72dfc892e5a1bff1fbb1e34169d832c3536fd28a
mikst <mika@kobakant.at>
2020-10-21 18:56:13 +0200

    channel name change

--- pyzmq/sub.py
+++ pyzmq/sub.py
@@ -3,7 +3,7 @@ xpub_addr = 'tcp://127.0.0.1:5555'
 context = zmq.Context()
 subscriberSocket = context.socket(zmq.SUB)
 subscriberSocket.connect(xpub_addr)
-subscriberSocket.setsockopt(zmq.SUBSCRIBE, b"sensors")
+subscriberSocket.setsockopt(zmq.SUBSCRIBE, b"xxx")
 while True:
     if subscriberSocket.poll(timeout=1000):
         message = subscriberSocket.recv_multipart()
