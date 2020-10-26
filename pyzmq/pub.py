0ecf26222b66a3a8055b90729b15fd59b40edb93
Ingo Randolf <ingo.randolf@servus.at>
2020-10-26 13:25:43 +0100

    use 192.168.0.10, and channel "sensors"

--- pyzmq/pub.py
+++ pyzmq/pub.py
@@ -1,9 +1,9 @@
 import zmq
-xsub_addr = 'tcp://127.0.0.1:5556'
+xsub_addr = 'tcp://192.168.0.10:5556'
 context = zmq.Context()
 publisherSocket = context.socket(zmq.PUB)
 publisherSocket.connect(xsub_addr)
-publishName = b"xxx"; #.encode('utf-8')
+publishName = b"sensors"; #.encode('utf-8')
 
 while True:
     message = input('input the message:')
--- pyzmq/sub.py
+++ pyzmq/sub.py
@@ -1,9 +1,9 @@
 import zmq
-xpub_addr = 'tcp://127.0.0.1:5555'
+xpub_addr = 'tcp://192.168.0.10:5555'
 context = zmq.Context()
 subscriberSocket = context.socket(zmq.SUB)
 subscriberSocket.connect(xpub_addr)
-subscriberSocket.setsockopt(zmq.SUBSCRIBE, b"xxx")
+subscriberSocket.setsockopt(zmq.SUBSCRIBE, b"sensors")
 while True:
     if subscriberSocket.poll(timeout=1000):
         message = subscriberSocket.recv_multipart()
